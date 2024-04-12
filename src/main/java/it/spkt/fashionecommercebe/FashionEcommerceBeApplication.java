1dd8c5dde44d7a1df3ba25398cd760d6478170de
Lê Quang Duẩn <20110619@student.hcmute.edu.vn>
2024-04-12 21:27:56 +0700

    init

---
+++ src/main/java/it/spkt/fashionecommercebe/FashionEcommerceBeApplication.java
@@ -0,0 +1,13 @@
+package it.spkt.fashionecommercebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionEcommerceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionEcommerceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/spkt/fashionecommercebe/FashionEcommerceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.spkt.fashionecommercebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionEcommerceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
